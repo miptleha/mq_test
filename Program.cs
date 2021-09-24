cf97ebbb6e1d1dca47ba378e559753e215ba0eb9
Aleksey Obukhov <miptleha@gmail.com>
2021-09-24 17:03:07 +0300

    add sources

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using IBM.WMQ;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace TestQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channelParams = ConfigurationManager.AppSettings.Get("ChannelInfo").Split(new[] { '/' });
+            string manager = ConfigurationManager.AppSettings.Get("QueueManagerName");
+            string queueName = ConfigurationManager.AppSettings.Get("QueueName");
+            string channel = channelParams[0];
+            string conn = channelParams[2];
+            string _userID = null;
+            string _pwd = null;
+
+            Console.WriteLine(string.Format("Connect to mq: manager={0}, channel={1}, conn={2}", manager, channel, conn));
+            MQEnvironment.UserId = _userID;
+            MQEnvironment.Password = _pwd;
+            MQQueueManager queueManager = null;
+            try
+            {
+                queueManager = new MQQueueManager(manager, channel, conn);
+                Console.WriteLine("Connected");
+            }
+            catch (MQException ex)
+            {
+                Console.WriteLine(string.Format("!!!Error connect to MQ: {0}, reason: {1}", ex.Message, ex.Reason));
+                return;
+            }
+
+            try
+            {
+                using (var queue = queueManager.AccessQueue(queueName, MQC.MQOO_INPUT_SHARED + MQC.MQOO_FAIL_IF_QUIESCING + MQC.MQOO_INQUIRE))
+                {
+                    Console.WriteLine("Number of messages: " + queue.CurrentDepth);
+                }
+            }
+            catch (MQException e)
+            {
+                Console.WriteLine(string.Format("!!!Error get currentDepth from queue {0}, error message: {1}, reason: {2}",
+                    queueName, e.Message, e.Reason));
+            }
+
+            queueManager.Disconnect();
+        }
+    }
+}
